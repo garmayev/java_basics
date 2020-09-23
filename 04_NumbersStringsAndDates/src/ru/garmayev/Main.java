9d11a09e01f25e4fb4cb10c87ec23a9403588d1d
Garmayev <garmayev@yandex.ru>
2020-09-23 10:10:26 +0800

    Homework 4.2

---
+++ 04_NumbersStringsAndDates/src/ru/garmayev/Main.java
@@ -0,0 +1,66 @@
+package ru.garmayev;
+
+public class Main {
+
+    public static void main(String[] args) {
+        minMaxByte();
+        minMaxInt();
+        minMaxShort();
+        minMaxLong();
+        minMaxFloat();
+        minMaxDouble();
+    }
+
+    public static void minMaxByte() {
+        System.out.println("Минимальное значение типа Byte");
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println("Максимальное значение типа Byte");
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println("=================================================");
+        System.out.println();
+    }
+    public static void minMaxInt() {
+        System.out.println("Минимальное значение типа Int");
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println("Максимальное значение типа Int");
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println("=================================================");
+        System.out.println();
+    }
+
+    public static void minMaxShort() {
+        System.out.println("Минимальное значение типа Short");
+        System.out.println(Short.MIN_VALUE);
+        System.out.println("Максимальное значение типа Short");
+        System.out.println(Short.MAX_VALUE);
+        System.out.println("=================================================");
+        System.out.println();
+    }
+
+    public static void minMaxLong() {
+        System.out.println("Минимальное значение типа Long");
+        System.out.println(Long.MIN_VALUE);
+        System.out.println("Максимальное значение типа Long");
+        System.out.println(Long.MAX_VALUE);
+        System.out.println("=================================================");
+        System.out.println();
+    }
+
+    public static void minMaxFloat() {
+        System.out.println("Минимальное значение типа Float");
+        System.out.println(Float.MIN_VALUE);
+        System.out.println("Максимальное значение типа Float");
+        System.out.println(Float.MAX_VALUE*1);
+        System.out.println("=================================================");
+        System.out.println();
+    }
+
+    public static void minMaxDouble() {
+        System.out.println("Минимальное значение типа Double");
+        System.out.println(Double.MIN_VALUE*1);
+        System.out.println("Максимальное значение типа Double");
+        System.out.println(Double.MAX_VALUE*1);
+        System.out.println("=================================================");
+        System.out.println();
+    }
+}
