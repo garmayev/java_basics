6798e2ee7277228d06500553b01ee018a4af3cc4
garmayev <garmayev@yandex.ru>
2020-10-03 17:15:39 +0800

    Homework #4.6.1. Исправленная, с использованием LocalDate

--- 04_NumbersStringsAndDates/Calendar/src/ru/garmayev/Main.java
+++ 04_NumbersStringsAndDates/Calendar/src/ru/garmayev/Main.java
@@ -2,23 +2,50 @@ package ru.garmayev;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.Calendar;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
+        /**
+         * version 1
+         */
+//        int day = 10;
+//        int month = 3;
+//        int year = 1990;
+//
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.set(year, month, day);
+//
+//        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+//        DateFormat dayFormat = new SimpleDateFormat("EEEE");
+//
+//        for (int i = 0; i < 31; i ++) {
+//            System.out.println(i + " - " + dateFormat.format(calendar.getTime()) + " - " + dayFormat.format(calendar.getTime()));
+//            calendar.add(Calendar.YEAR, 1);
+//        }
+
+        /**
+         * v1 Update 1 (3.10.2020)
+         */
         int day = 10;
-        int month = 3;
+        int month = 4;
         int year = 1990;
 
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(year, month, day);
+        int count = 0;
 
-        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        DateFormat dayFormat = new SimpleDateFormat("EEEE");
+        LocalDate birthday, today;
+        birthday = LocalDate.of(year, month, day);
+        today = LocalDate.now();
 
-        for (int i = 0; i < 31; i ++) {
-            System.out.println(i + " - " + dateFormat.format(calendar.getTime()) + " - " + dayFormat.format(calendar.getTime()));
-            calendar.add(Calendar.YEAR, 1);
+        while (today.isAfter(birthday)) {
+            String formattedDate = DateTimeFormatter.ofPattern("dd.MM.yyyy EEEE", new Locale("ru")).format(birthday);
+            System.out.println(count + " - " + formattedDate);
+            count++;
+            birthday = birthday.plusYears(1);
         }
     }
 }
